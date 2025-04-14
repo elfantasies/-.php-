fcfd517e09eb7d8a08c8c9b9227b670b304b5d49
elfantasies <asaelmatthew13@icloud.com>
2025-04-14 18:34:59 +0800

    Update 8.counter.php

--- 8.counter.php
+++ 8.counter.php
@@ -1,20 +1,10 @@
 <?php
-    error_reporting(0);
-    $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
-    $result=mysqli_query($conn, "select * from bulletin");
-    echo "<table border=2><tr><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";
-    while ($row=mysqli_fetch_array($result)){
-        echo "<tr><td>";
-        echo $row["bid"];
-        echo "</td><td>";
-        echo $row["type"];
-        echo "</td><td>"; 
-        echo $row["title"];
-        echo "</td><td>";
-        echo $row["content"]; 
-        echo "</td><td>";
-        echo $row["time"];
-        echo "</td></tr>";
-    }
-    echo "</table>"
+    session_start();
+    if (!isset($_SESSION["counter"]))
+        $_SESSION["counter"]=1;
+    else
+        $_SESSION["counter"]++;
+
+    echo "counter=".$_SESSION["counter"];
+    echo "<br><a href=9.reset_counter.php>重置counter</a>";
 ?>
