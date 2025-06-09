9fcf4e31fc34c128e85f3a0df66aecd40921a53c
elfantasies <asaelmatthew13@icloud.com>
2025-06-09 11:59:49 +0800

    Create 27.bulletin_edit.php

---
+++ 27.bulletin_edit.php
@@ -0,0 +1,24 @@
+#Process Bulletin Edit Request
+<?php
+
+    error_reporting(0);
+    session_start();
+    if (!$_SESSION["id"]) {
+        echo "Please log in your account";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    }
+    else{   
+        $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+      #Execute Update Query
+        if (!mysqli_query($conn, "update bulletin set title='{$_POST['title']}',content='{$_POST['content']}',time='{$_POST['time']}',type={$_POST['type']} where bid='{$_POST['bid']}'")){
+            echo "修改錯誤";
+            echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
+        }
+        #On Success: Show Confirmation
+        else{
+            echo "修改成功，三秒鐘後回到佈告欄列表";
+            echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
+        }
+    }
+
+?>
